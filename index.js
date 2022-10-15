47ea834143b03082877d8016fc430c99b8293854
p-o-s <6580442+p-o-s@users.noreply.github.com>
2022-10-16 01:04:54 +0200

    .

--- index.js
+++ index.js
@@ -285,13 +285,11 @@ app.get('/roles', async(req,res) => {
     let roles = await discord_api.get(`/guilds/${GUILD_ID}/roles`) // kingofthehill-id: 1030927052964106250
     let members = await discord_api.get(`/guilds/${GUILD_ID}/members?limit=100`)
     let kothMembers = members.data.filter(member => member.roles.length && member.roles.includes('1030927052964106250'))
-    console.log(roles.data)
-    console.log(members)
+    //console.log(roles.data)
+    //console.log(members)
     console.log('koth members: ', kothMembers)
 
-    console.log(`king of the hill: ${kothMembers[kothMembers.length * Math.random() | 0].user.username}`)
-
-    return res.send('OK')
+    return res.send(`king of the hill: ${kothMembers[kothMembers.length * Math.random() | 0].user.username}`)
   }catch(e){
     console.log(e)
     return res.send('ERROR')
