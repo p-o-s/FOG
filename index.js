1a0ea3a4f191a57af1c1270077e8c0757345b845
p-o-s <6580442+p-o-s@users.noreply.github.com>
2022-10-18 18:50:50 +0200

    .

--- index.js
+++ index.js
@@ -288,15 +288,17 @@ app.get('/roles', async(req,res) => {
     //console.log(roles.data)
     //console.log(members)
     console.log('koth members: ', kothMembers)
+    console.log(channelMessages.data);
 
     // delete old koth message
     let channelMessages = await discord_api.get(`/channels/${KUSCHELECKE}/messages?limit=100`)
-    
+    let filteredMessages = channelMessages.data.filter(msg => msg.author.id === '922139349603209267' && !msg.interaction)
+    // father of god id 922139349603209267
+
     // send new koth message
     // POST  /channels/{channel.id}/messages
-    let kothAnnouncement = await discord_api.post(`/channels/${KUSCHELECKE}/messages`, {content: 'test'})
+    let kothAnnouncement = await discord_api.post(`/channels/${KUSCHELECKE}/messages`, {content: `\u200atest`})
     
-    console.log(channelMessages.data);
     return res.send(`king of the hill: ${kothMembers[kothMembers.length * Math.random() | 0].user.username}`)
   }catch(e){
     console.log(e)
