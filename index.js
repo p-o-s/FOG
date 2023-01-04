cefadf2d373c6ee77e5df5ab4e67db16591cde74
p-o-s <6580442+p-o-s@users.noreply.github.com>
2023-01-04 18:00:28 +0100

    .

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         console.log(e)
       }
 
-      console.log(test)
+      console.log('test')
       console.log(res);
 
       return res.send({
